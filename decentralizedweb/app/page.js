c19d97d0390fed058ad196a8060e0bac58a0b708
Anagha098 <anaghamahure@gmail.com>
2024-04-03 15:11:24 +0530

    frontend started

---
+++ decentralizedweb/app/page.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout/Layout";
+import LoginPage from "@/components/LoginPage";
+// import { headers } from "next/headers";
+export default function Home() {
+  return (
+
+    <Layout>
+      <LoginPage>
+
+      </LoginPage>
+    </Layout>
+  );
+}
