ea6e9e943d3eee92842552fd04c58b550697fbc1
Vedang Khedekar <103302551+vedang1010@users.noreply.github.com>
2024-10-13 10:32:55 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -8,8 +8,9 @@ import { getAuth } from "firebase/auth";
 import { initFirebase } from "@/Config/firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getDatabase, ref, onValue } from "firebase/database";
-import {Toast} from "react-toastify"
-const dashboard = () => {
+import { Toast } from "react-toastify"
+
+function Dashboard() {
   const app = initFirebase();
   const database = getDatabase(app);
   const router = useRouter(); // Initialize useRouter hook
@@ -17,73 +18,51 @@ const dashboard = () => {
   const [user, loading] = useAuthState(auth);
   const [websites, setWebsites] = useState([]);
 
-
-  // console.log(user.uid)
   useEffect(() => {
     const rootRef = ref(database, "users");
     onValue(rootRef, (snapshot) => {
       const users = snapshot.val();
       const updatedWebsites = [];
       for (const userId in users) {
-        // if(userId===user.uid){
         const userData = users[userId];
         const { domain, cid } = userData?.uploads || {}; // Access domain and cid from user's uploads data
         if (domain && cid) {
           updatedWebsites.push({ userId, domain, cid });
-          // }
         }
       }
-
       setWebsites(updatedWebsites);
     });
-
   }, [database]);
-  var loggedInUserWebsite=""
-  var loggedInUserDomain=""
-  
-var loggedInUserCID=""
-  if (user) {
-     loggedInUserWebsite = websites.find(website => website.userId === user.uid);
-     loggedInUserDomain = loggedInUserWebsite ? loggedInUserWebsite.domain : ''; // Domain of the logged-in user
-     loggedInUserCID = loggedInUserWebsite ? loggedInUserWebsite.cid : ''; // CID of the logged-in user
-  }
-  // console.log(app);
-  // const callApi=async()=>{
-  //   const token = await user.getIdToken();
 
-  //   const echoEndpoint = "https://jwtecho.pixegami.io";
-  //   const certStr=""
-  //   const audience =""
-  //   const verifigation='${echoEndpoint}/verify?audience=${audience}&cert_str=${certStr}'
-  //   const requestInfo={
-  //     headers:{
-  //       Authorization:"Bearer "+token,
-  //     },
-  //   }
-  //   const response=await fetch(echoEndpoint,requestInfo)
-  //   const responseBody=await response.json()
-  //   console.log(token)
-  //   console.log(responseBody)
-  // }
+  var loggedInUserWebsite = "";
+  var loggedInUserDomain = "";
+  var loggedInUserCID = "";
+
+  if (user) {
+    loggedInUserWebsite = websites.find(website => website.userId === user.uid);
+    loggedInUserDomain = loggedInUserWebsite ? loggedInUserWebsite.domain : ''; // Domain of the logged-in user
+    loggedInUserCID = loggedInUserWebsite ? loggedInUserWebsite.cid : ''; // CID of the logged-in user
+  }
+
   const handleSignOut = () => {
-    // Add your additional action here
     console.log("Additional action before sign out");
-    // Call auth.signOut()
     router.push("/login")
     auth.signOut();
   };
+
   if (loading) {
     return <div>Loading....</div>;
   }
+  
   if (!user) {
     router.push('/login');
-
     return <div>Please sign in to continue</div>;
   }
+
   const handleFileManagerClick = () => {
     router.push('/file'); // Navigate to the files page
   };
-  console.log(websites);
+
   return (
     <Layout>
       <DashboardTopLeft>
@@ -102,7 +81,6 @@ var loggedInUserCID=""
           <StyledImage src="/ws3.jpg" alt="ws3" width={1000} height={1000} />
           <StyledImage src="/ws1.png" alt="ws1" width={1000} height={1000} />
           <StyledImage src="/ws4.jpg" alt="ws4" width={1000} height={1000} />
-
         </div>
       </ImageContainer>
 
@@ -110,58 +88,52 @@ var loggedInUserCID=""
         <FileManager onClick={handleFileManagerClick}>File Manager</FileManager>
         <VisitorCount>
           <Content onClick={() => router.push('/ide')}>Code Editor</Content>
-          <Logs ></Logs>
+          <Logs></Logs>
         </VisitorCount>
       </DashboardContent>
-
     </Layout>
   )
 }
 
-export default dashboard
+export default Dashboard;
 
 const CID = styled.a`
-color: blue;
-text-decoration: none;
-margin-left: 10px;
+  color: blue;
+  text-decoration: none;
+  margin-left: 10px;
 
-&:hover {
-  text-decoration: underline;
-}
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const ImageContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    padding: 50px;
-    width: 100%;
-    height: 250px;
-    align-items: flex-start;
-
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 50px;
+  width: 100%;
+  height: 250px;
+  align-items: flex-start;
 `;
 
 const StyledImage = styled.img`
-  /* Add your styles here */
   border-radius: 5px;
   height: 200px;
   width: auto;
   margin: 0;
   &:first-child {
-    margin-right: 0px; /* Add some space between the images */
+    margin-right: 0px;
   }
-  /* margin-left: 50px; */ 
   &:hover {
-    transform: scale(1.2); /* Scale the image up by 10% on hover */
+    transform: scale(1.2);
     transition: transform 0.2s ease;
-}
-
+  }
 `;
 
 const Logs = styled.div`
   font-size: 12px;
-  cursor: pointer; /* Ensure the cursor changes on hover */
-  
+  cursor: pointer;
   &:hover {
     text-decoration: underline;
     color: blue;
@@ -169,18 +141,16 @@ const Logs = styled.div`
 `;
 
 const Content = styled.div`
-    font-size: 16px;
-`
-
+  font-size: 16px;
+`;
 
 const DashboardTopLeft = styled.div`
   flex: 1;
-  /* left: 0; */
   margin-right: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const DashboardContent = styled.div`
   flex: 2;
@@ -191,15 +161,14 @@ const DashboardContent = styled.div`
   flex-direction: column;
   align-items: center;
   flex-direction: row;
-  
-`
+`;
 
 const Header = styled.h1`
   padding: 20px;
   font-size: 24px;
   font-weight: bold;
   align-items: center;
-  `
+`;
 
 const Subheader = styled.p`
   font-size: 14px;
@@ -207,15 +176,13 @@ const Subheader = styled.p`
   justify-content: center;
   align-items: center;
   color: #666;
-`
+`;
 
 const FileManager = styled.button`
   background-color: #f2f2f2;
   padding: 10px;
   border-radius: 5px;
-  display: flex;
   cursor: pointer;
-  
   justify-content: center;
   align-items: center;
   margin: 20px;
@@ -225,10 +192,10 @@ const FileManager = styled.button`
   background-color: ${(props) => props.theme.bgColor};
   color: ${(props) => props.theme.color};
   &:hover {
-    transform: scale(1.1); /* Scale the image up by 10% on hover */
+    transform: scale(1.1);
     transition: transform 0.2s ease;
-}
-  `
+  }
+`;
 
 const VisitorCount = styled.div`
   background-color: #f2f2f2;
@@ -240,19 +207,18 @@ const VisitorCount = styled.div`
   border: 1px solid;
   display: flex;
   align-items: center;
-  display: flex; 
   flex-direction: column;
   justify-content: center;
   background-color: ${(props) => props.theme.bgColor};
   color: ${(props) => props.theme.color};
   &:hover {
-    transform: scale(1.1); /* Scale the image up by 10% on hover */
+    transform: scale(1.1);
     transition: transform 0.2s ease;
-}
-`
-const SignOutContainer = styled.button`
+  }
+`;
 
-   cursor: pointer;
+const SignOutContainer = styled.button`
+  cursor: pointer;
   color: #fff;
   font-size: 16px;
   margin: 8px;
@@ -260,7 +226,5 @@ const SignOutContainer = styled.button`
   text-transform: uppercase;
   padding: 10px 20px;
   border-radius: 5px;
-  /* background-color: #006c87; */
-  background-color: ${(props)=>props.theme.bgDiv};
-
-`
+  background-color: ${(props) => props.theme.bgDiv};
+`;
